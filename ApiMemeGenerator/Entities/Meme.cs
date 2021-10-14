21a32828323603b5a8ccc4593127592d071cbcb9
Guillermo Munoz Macias <guillermo.munoz@globant.com>
2021-10-14 16:29:18 -0500

    Meme Generator with business not in controller

---
+++ ApiMemeGenerator/Entities/Meme.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiMemeGenerator.Entities
+{
+    public class Meme
+    {
+        public string URL { get; set; }
+
+        public string Notificacion {  get; set; }
+    }
+}
---
+++ ApiMemeGenerator/Enum/UbicacionTexto.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiMemeGenerator.Enum
+{
+    public enum UbicacionTexto
+    {
+        Arriba = 0, TresCuarto = 1, Medio = 2, UnTercio= 3, Abajo = 4
+    }
+}
